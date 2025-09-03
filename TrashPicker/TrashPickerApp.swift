9a95867869f1ab48553db43425b2dce8e433cf6e
ZainLE <latifzain40@gmail.com>
2025-09-03 19:05:54 +0200

    Initial Commit

---
+++ TrashPicker/TrashPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrashPickerApp.swift
+//  TrashPicker
+//
+//  Created by Zain Latif  on 3/9/25.
+//
+
+import SwiftUI
+
+@main
+struct TrashPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TrashPickerTests/TrashPickerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrashPickerTests.swift
+//  TrashPickerTests
+//
+//  Created by Zain Latif  on 3/9/25.
+//
+
+import Testing
+@testable import TrashPicker
+
+struct TrashPickerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
